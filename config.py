d5aef253139cc2e8e859181dd0033a0e89bc4238
Yara Lacerda <128132279+Yara-56@users.noreply.github.com>
2024-06-10 09:11:19 -0300

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ SECRET_KEY = 'alura'
 SQLALCHEMY_DATABASE_URI = \
     '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
         SGBD='mysql+mysqlconnector',
-        usuario='root',
-        senha='1980',
+        usuario='#',
+        senha='#',
         servidor='localhost',
         database='jogoteca'
     )
